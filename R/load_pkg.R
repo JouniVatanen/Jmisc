daef6f780dc179056c162143ec2cf8eb9eca9a93
Jouni Vatanen <jounipvatanen@gmail.com>
2018-09-12 08:42:19 +0300

    new function load_pkg and create_project

---
+++ R/load_pkg.R
@@ -0,0 +1,39 @@
+#' load_pkg
+#'
+#' This function installs and loads your favourite packages.
+#' @param packages
+#' @keywords packages
+#' @export
+#' @examples
+#' load_pkg(packages = "long")
+#' @export
+
+load_pkg <- function(packages = "long") {
+
+  #select favourite packages
+  if (length(packages) == 1L && packages == "long") {
+    packages <- c("readxl", "tidyverse", "tidyr", "modelr", "broom",
+      "data.table", "magrittr", "lubridate", "desctable", "Hmisc", "GGally",
+      "knitr", "kableExtra", "formattable","gridExtra", "FactoMineR",
+      "factoextra", "ClustOfVar", "caret", "psych", "ggrepel", "pROC", "DT",
+      "webshot")
+  } else if (length(packages) == 1L && packages == "short") {
+    packages <- c("readxl", "tidyverse", "tidyr", "data.table", "magrittr",
+      "lubridate", "knitr")
+  }
+
+  packagecheck <- match(packages, utils::installed.packages()[,1])
+
+  packagestoinstall <- packages[is.na(packagecheck)]
+
+  if(length(packagestoinstall) > 0L ) {
+    utils::install.packages(packagestoinstall, repos = "http://cran.wu.ac.at/")
+  } else {
+    print("All requested packages already installed")
+  }
+
+  for(package in packages) {
+    suppressPackageStartupMessages(
+      library(package, character.only = TRUE, quietly = TRUE))
+  }
+}
