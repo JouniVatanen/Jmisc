cb10d180d140c4d3e4b6776c27c82ed72cdaed70
Jouni Vatanen <jounipvatanen@gmail.com>
2018-09-30 16:32:17 +0300

    Ilmarinen themes

--- R/ilmarinen_cols.R
+++ R/ilmarinen_cols.R
@@ -1,18 +1,20 @@
-# Ilmarinen corporate colors
-ilmarinen_colors <- c(
-  `dark blue` = "#00274B",
-  `pink` = "#F37B8A",
-  `light blue` = "#84D2DF",
-  `yellow` = "#FFE180",
-  `orange` = "#DE6328",
-  `blue` = "#3A8DA9",
-  `grey` = "#A6A6A6")
-
 #' Function to extract Ilmarinen colors as hex codes
 #'
 #' @param ... Character names of ilmarinen_colors
+#' @export
 
 ilmarinen_cols <- function(...) {
+
+  # Ilmarinen corporate colors
+  ilmarinen_colors <- c(
+    `dark blue` = "#00274B",
+    `pink` = "#F37B8A",
+    `light blue` = "#84D2DF",
+    `yellow` = "#FFE180",
+    `orange` = "#DE6328",
+    `blue` = "#3A8DA9",
+    `grey` = "#A6A6A6")
+
   cols <- c(...)
 
   if (is.null(cols))
@@ -21,22 +23,25 @@ ilmarinen_cols <- function(...) {
   ilmarinen_colors[cols]
 }
 
-# Ilmarinen palettes
-ilmarinen_palettes <- list(
-  `main`  = ilmarinen_cols("dark blue", "pink", "light blue", "yellow"),
-  `cool`  = ilmarinen_cols("dark blue", "blue", "light blue"),
-  `hot`   = ilmarinen_cols("orange", "pink", "yellow"),
-  `mixed` = ilmarinen_cols("light blue", "yellow", "pink"),
-  `all`   = ilmarinen_cols("dark blue", "pink", "light blue", "yellow", "orange", "blue", "grey")
-)
-
 #' Return function to interpolate Ilmarinen color palette
 #'
 #' @param palette Character name of palette in ilmarinen_palettes
 #' @param reverse Boolean indicating whether the palette should be reversed
 #' @param ... Additional arguments to pass to colorRampPalette()
+#' @export
 
 ilmarinen_pal <- function(palette = "main", reverse = FALSE, ...) {
+
+  # Ilmarinen palettes
+  ilmarinen_palettes <- list(
+    `main`  = ilmarinen_cols("dark blue", "pink", "light blue", "yellow"),
+    `cool`  = ilmarinen_cols("dark blue", "blue", "light blue"),
+    `hot`   = ilmarinen_cols("orange", "pink", "yellow"),
+    `mixed` = ilmarinen_cols("light blue", "yellow", "pink"),
+    `all`   = ilmarinen_cols("dark blue", "pink", "light blue", "yellow", "orange", "blue", "grey")
+  )
+
+
   pal <- ilmarinen_palettes[[palette]]
 
   if (reverse) pal <- rev(pal)
@@ -52,6 +57,8 @@ ilmarinen_pal <- function(palette = "main", reverse = FALSE, ...) {
 #' @param ... Additional arguments passed to discrete_scale() or
 #'            scale_color_gradientn(), used respectively when discrete is TRUE or FALSE
 #'
+#' @export
+
 scale_color_ilmarinen <- function(palette = "main", discrete = TRUE, reverse = FALSE, ...) {
   pal <- ilmarinen_pal(palette = palette, reverse = reverse)
 
@@ -69,6 +76,7 @@ scale_color_ilmarinen <- function(palette = "main", discrete = TRUE, reverse = F
 #' @param reverse Boolean indicating whether the palette should be reversed
 #' @param ... Additional arguments passed to discrete_scale() or
 #'            scale_fill_gradientn(), used respectively when discrete is TRUE or FALSE
+#' @export
 
 scale_fill_ilmarinen <- function(palette = "main", discrete = TRUE, reverse = FALSE, ...) {
   pal <- ilmarinen_pal(palette = palette, reverse = reverse)
