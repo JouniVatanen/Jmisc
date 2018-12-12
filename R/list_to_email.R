e008a8de57ee89674685e8ceada0770c9fe4702d
Jouni Vatanen <jounipvatanen@gmail.com>
2018-12-12 15:53:01 +0200

    upgraded functionality

--- R/list_to_email.R
+++ R/list_to_email.R
@@ -3,17 +3,26 @@
 #' This function helps you to save O365 type email addresses from list
 #' @param source From where to read the data. Default: clipboard.
 #' @param reverse Reverse the order of the first and the last name. Default: FALSE.
+#' @param sep Separator, if data is not from clipboard. Default: tab.
 #' @keywords email list
 #' @examples
 #' list_to_email()
 #' @export
 
-list_to_email <- function(source = readClipboard(), reverse = FALSE) {
+list_to_email <- function(source = readClipboard(), sep = "\t", reverse = FALSE) {
+
+  # Copy from clipboard
+  if (source == readClipboard()) {
+    source <- stringi::stri_replace_all_regex(source,
+      c(";", ">", " <"),
+      c("\n", "", "\t"),
+      vectorize_all = FALSE)
+  }
 
   # Load clipboard to and transform the data with regex
-  table <- read.table(text =
-    gsub(";","\n", gsub(">", "", source, perl = TRUE), perl = TRUE),
-    sep = "<",
+  table <- read.table(
+    text = source,
+    sep = sep,
     strip.white = TRUE,
     header = FALSE,
     col.names = c("name", "email"))
