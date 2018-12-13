da66ce2dd6186c6b139feeb61a7f31646dc2c382
Jouni Vatanen <jounipvatanen@gmail.com>
2018-12-13 14:58:16 +0200

    added functionality

--- R/list_to_email.R
+++ R/list_to_email.R
@@ -1,7 +1,7 @@
 #' list_to_email
 #'
 #' This function helps you to save O365 type email addresses from list
-#' @param source From where to read the data. Default: clipboard.
+#' @param email From where to read the data. Default: clipboard.
 #' @param reverse Reverse the order of the first and the last name. Default: FALSE.
 #' @param sep Separator, if data is not from clipboard. Default: tab.
 #' @keywords email list
@@ -9,23 +9,39 @@
 #' list_to_email()
 #' @export
 
-list_to_email <- function(source = readClipboard(), sep = "\t", reverse = FALSE) {
+list_to_email <- function(email = NULL, sep = "\t", reverse = FALSE) {
 
-  # Copy from clipboard
-  if (source == readClipboard()) {
-    source <- stringi::stri_replace_all_regex(source,
+  # If email ending is not defined
+  if (is.null(email)) {
+
+    # Copy from Outlook like clipboard
+    source <- stringi::stri_replace_all_regex(tolower(readClipboard()),
       c(";", ">", " <"),
       c("\n", "", "\t"),
       vectorize_all = FALSE)
+
+    col.names <- c("name", "email")
+
+  } else {
+  # If email ending is defined
+    # Copy from text and put
+    source <- stringi::stri_replace_all_regex(tolower(readClipboard()),
+      c("'", "å|ä", "ö", "é|è|ë|ê", "^(\\S*).?(\\S*)$"),
+      c("" , "a"  , "o", "e"      , "$2.$1"),
+      vectorize_all = FALSE) %>%
+      paste0(., "@", email)
+
+    col.names <- "email"
   }
 
-  # Load clipboard to and transform the data with regex
+  # Read to dataframe
   table <- read.table(
     text = source,
     sep = sep,
     strip.white = TRUE,
     header = FALSE,
-    col.names = c("name", "email"))
+    col.names = col.names,
+    quote = "\"")
 
   # Reverse order of the first name and last name
   if (reverse) {
