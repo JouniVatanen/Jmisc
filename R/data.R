63f202e12c08c70e1553de4cc5f6a9c696189d45
Jouni Vatanen <jounipvatanen@gmail.com>
2019-08-01 09:46:36 +0300

    added postnumbers and people names data and their descriptions

---
+++ R/data.R
@@ -0,0 +1,35 @@
+#' Finnish postnumbers 2016
+#'
+#' Data from Posti about Finnish postnumbers and their metadata.
+#' Data is from 2016. Data can be downloaded from:
+#' https://beta.posti.fi/fi/asiakastuki/postinumerotiedostot
+#'
+#' @format A data frame with 939 rows and 10 variables:
+#' \describe{
+#'   \item{postinumero}{Postnumber depricated to first three characters}
+#'   \item{maakunta}{Area codes divided by province}
+#'   \item{maakunnan_nimi}{Area names divided by province}
+#'   \item{kuntaryhma}{Area codes divided by area type}
+#'   \item{kuntaryhman_nimi}{Area names divided by area type}
+#'   \item{seutukunta}{Area codes divided by great area}
+#'   \item{seutukunnan_nimi}{Area names divided by great area}
+#'   \item{suuralue}{Area codes divided by NUTS2}
+#'   \item{suuralueen_nimi}{Area names divided by NUTS2}
+#'   \item{alue}{Area names divided by compass point}
+#' }
+"fi.postnumbers.2016"
+
+#' Finnish names
+#'
+#' Data from Väestörekisterikeskus about Finnish first and last names by gender
+#' and their how popular they are. Data is from 2016. Data can be downloaded from:
+#' https://www.avoindata.fi/data/fi/dataset/none
+#'
+#' @format A data frame with 34935 rows and 4 variables:
+#' \describe{
+#'   \item{name}{name of a person}
+#'   \item{n}{popularity of the name}
+#'   \item{gender}{Male = M, Female = F, NA in lastnames}
+#'   \item{type}{Firstname = F, lastname = L}
+#' }
+"fi.people.names"
---
+++ data-raw/create_data.R
@@ -0,0 +1,45 @@
+# Load libraries
+library(dplyr)
+library(vroom)
+library(stringr)
+library(janitor)
+library(devtools)
+
+# Load postinumbers data
+fi.postnumbers.2016 <- vroom(
+  "./data-raw/postnumber_2016.txt", skip = 4,
+  col_types = cols(.default = col_character()),
+  .name_repair = janitor::make_clean_names
+  ) %>% {
+    # Shorten postnumbers to first three characters
+    bind_rows(
+      mutate(., postinumero = paste0(str_sub(postinumeroalue, 1, 2), "0")) %>%
+        distinct(postinumero, .keep_all = TRUE),
+      mutate(., postinumero = str_sub(postinumeroalue, 1, 3)))
+    } %>%
+  # Add new area variable by Maakunta
+  mutate_at(vars(maakunta), list(alue = ~case_when(
+    . %in% c("01", "05", "07", "08") ~ "Etelä-Suomi",
+    . %in% c("02", "04", "06", "13", "14", "15", "16") ~ "Länsi-Suomi",
+    . %in% c("09", "10", "11", "12") ~ "Itä-Suomi",
+    . %in% c("17", "18", "19") ~ "Pohjois-Suomi",
+    TRUE ~ "Tuntematon"))) %>%
+  # Select
+  select(
+    "postinumero", "maakunta", "maakunnan_nimi", "kuntaryhma",
+    "kuntaryhman_nimi", "seutukunta", "seutukunnan_nimi", "suuralue",
+    "suuralueen_nimi", "alue"
+    ) %>%
+  distinct(postinumero, .keep_all = TRUE)
+
+# Load people names data
+fi.people.names <- vroom(
+  "./data-raw/people_names.txt",
+  .name_repair = janitor::make_clean_names
+    ) %>%
+  # Turn n from character to integer
+  mutate_at(vars(n), ~(str_replace_all(., " ", "") %>% as.integer))
+
+# Save data to use in package: Jmisc
+use_data(fi.postnumbers.2016, overwrite = TRUE)
+use_data(fi.people.names, overwrite = TRUE)
