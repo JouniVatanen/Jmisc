1d35a1188adeaf2646c950f58b40ccffde764620
Jouni Vatanen <jounipvatanen@gmail.com>
2019-08-05 08:28:34 +0300

    deleted create_project function

--- Jmisc.R
+++ Jmisc.R
@@ -4,15 +4,13 @@ if (!require("checkpoint")) install.packages("checkpoint"); library(checkpoint)
 # Use the date few weeks after R-3.6.1. was released
 checkpoint("2019-07-28", R.version = "3.6.1",
            checkpointLocation = Sys.getenv("USERPROFILE"))
-library(roxygen2)
-library(devtools)
 
 # Create package documentation
-document()
+devtools::document()
 
 # Install package
 setwd("..")
-install("jmisc")
+devtools::install("jmisc")
 setwd("./jmisc")
 
 # Commit changes and push the files to the github
