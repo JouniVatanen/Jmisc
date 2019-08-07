8148a7a46837956175520b6f5042f292a7d470e8
Jouni Vatanen <jounipvatanen@gmail.com>
2019-08-07 11:16:37 +0300

    changed package name

--- stools.R
+++ stools.R
@@ -16,4 +16,4 @@ setwd("./stools")
 # Commit changes and push the files to the github
 # 1. Commit changes shell "git add .;git commit -m 'comment'" OR Rstudio UI
 # 2. Push changes either shell "git push" OR Rstudio UI
-# 3. Install from github with devtools::install_github(JouniVatanen/Jmisc)
+# 3. Install from github with devtools::install_github(JouniVatanen/stools)
