2aa9e6d063649a4aebe7403b5114a119ad9b3845
Jouni Vatanen <jounipvatanen@gmail.com>
2019-08-11 16:02:29 +0300

    added readr as dependency

--- data-raw/create_data.R
+++ data-raw/create_data.R
@@ -8,23 +8,21 @@ checkpoint("2019-07-28", R.version = "3.6.1",
 # Load libraries
 library(dplyr)
 library(tidyr)
-library(vroom)
-library(stringr)
-library(janitor)
+library(stringi)
 library(devtools)
 library(tibble)
 
 # Load postinumbers data
-fi_postnumbers_2016 <- vroom(
+fi_postnumbers_2016 <- vroom::vroom(
   "./data-raw/postnumbers_2016.txt", skip = 4,
-  col_types = cols(.default = col_character()),
+  col_types = vroom::cols(.default = vroom::col_character()),
   .name_repair = janitor::make_clean_names
   ) %>% {
     # Shorten postnumbers to first three characters
     bind_rows(
-      mutate(., postinumero = paste0(str_sub(postinumeroalue, 1, 2), "0")) %>%
+      mutate(., postinumero = paste0(stri_sub(postinumeroalue, 1, 2), "0")) %>%
         distinct(postinumero, .keep_all = TRUE),
-      mutate(., postinumero = str_sub(postinumeroalue, 1, 3)))
+      mutate(., postinumero = stri_sub(postinumeroalue, 1, 3)))
     } %>%
   # Add new area variable by Maakunta
   mutate_at(vars(maakunta), list(alue = ~case_when(
@@ -42,7 +40,7 @@ fi_postnumbers_2016 <- vroom(
   distinct(postinumero, .keep_all = TRUE)
 
 # Load industries data
-fi_industries_2008 <- vroom(
+fi_industries_2008 <- vroom::vroom(
   "./data-raw/industries_2008.txt", skip = 3,
   .name_repair = janitor::make_clean_names) %>%
   filter(taso %in% c(1, 2)) %>%
@@ -60,12 +58,12 @@ fi_industries_2008 <- vroom(
   select(tol_0, tol_1, tol_1_name, tol_2, tol_2_name)
 
 # Load people names data
-fi_people_names <- vroom(
+fi_people_names <- vroom::vroom(
   "./data-raw/people_names.txt",
   .name_repair = janitor::make_clean_names
     ) %>%
   # Turn n from character to integer
-  mutate_at(vars(n), ~(str_replace_all(., " ", "") %>% as.integer)) %>%
+  mutate_at(vars(n), ~(stri_replace_all_regex(.x, " ", "") %>% as.integer)) %>%
   arrange(desc(n))
 
 # Create people names to remove
