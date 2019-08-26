41bb53ad903b628b1db5c012247df8f5426c8b36
Jouni Vatanen <jounipvatanen@gmail.com>
2019-08-26 08:45:47 +0300

    Parameters for plot_wordcloud

--- R/plot_wordcloud.R
+++ R/plot_wordcloud.R
@@ -4,6 +4,11 @@
 #' @param x Vector of sentences or words.
 #' @param output Output path.
 #' @param scale A vector of length 2 indicating the range of the size of the words.
+#' @param res The nominal resolution in ppi which will be recorded in the bitmap
+#' file, if a positive integer. Also used for units other than the default.
+#' If not specified, taken as 72 ppi to set the size of text and line widths.
+#' @param fixed.asp if TRUE, the aspect ratio is fixed. Variable aspect ratio
+#' only supported if rot.per==0
 #' @param removePunctuation Remove punctuation. Default: TRUE.
 #' @param removeNumbers Remove numbers. Default: TRUE.
 #' @param stripWhitespace Remove whitespace. Default: TRUE.
@@ -21,8 +26,10 @@
 #' @importFrom wordcloud wordcloud
 #' @importFrom graphics par
 #' @importFrom grDevices dev.off png
+#' @importFrom dplyr pull
 
 plot_wordcloud <- function(x, output = "./wordcloud.png", scale = c(4, .5),
+                           res = 100, fixed.asp = FALSE,
                            removePunctuation = TRUE, removeNumbers = TRUE,
                            stripWhitespace = TRUE, colors = ilmarinen_cols()) {
 
@@ -40,9 +47,9 @@ plot_wordcloud <- function(x, output = "./wordcloud.png", scale = c(4, .5),
   df <- data.frame(word = names(vec), freq = vec)
 
   # Save as a file
-  png(output, width = 1280, height = 800, units = "px", res = 100)
+  png(output, width = 1280, height = 800, units = "px", res = res)
   par(mar = rep(0, 4))
   wordcloud(df$word, df$freq, scale = scale, min.freq = 1, max.words = 200,
-            rot.per = 0, colors = colors)
+            rot.per = 0, colors = colors, fixed.asp = fixed.asp)
   dev.off()
 }
