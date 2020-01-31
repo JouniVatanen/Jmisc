859a0f5f98515c73303979721fa6cd7bc0a5d7ae
Jouni Vatanen <jounipvatanen@gmail.com>
2020-01-31 14:37:38 +0200

    new functions write_csv_fi and write_row2file

--- stools.R
+++ stools.R
@@ -1,8 +1,8 @@
 # Load required packages
 if (!require("checkpoint")) install.packages("checkpoint")
 
-# Use the date few weeks after R-3.6.1. was released
-checkpoint::checkpoint("2019-07-28", R.version = "3.6.1",
+# Use the date few weeks after R-3.6.2. was released
+checkpoint::checkpoint("2019-12-30", R.version = "3.6.2",
                        checkpointLocation = Sys.getenv("USERPROFILE"))
 
 # Document and install package
@@ -14,3 +14,4 @@ install()
 # 1. Commit changes shell "git add .;git commit -m 'comment'" OR Rstudio UI
 # 2. Push changes either shell "git push" OR Rstudio UI
 # 3. Install from github with devtools::install_github(JouniVatanen/stools)
+
