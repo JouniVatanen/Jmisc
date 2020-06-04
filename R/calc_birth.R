be3f442ac1add0821a5aae2b0a96ec664cb7a383
Jouni Vatanen <jounipvatanen@gmail.com>
2020-06-04 12:33:24 +0300

    faster calculate birth from Finnish social security number

--- R/calc_birth.R
+++ R/calc_birth.R
@@ -1,21 +1,30 @@
 #' calc_birth
 #'
-#' Calculate birth date from social security number and return POSIXct
+#' Calculate birth date from Finnish social security number and return POSIXct
 #' @param x Choose string from which to calculate.
 #' @keywords social security, birth, date
 #' @examples
-#' calc_birth("123456-123S")
+#' calc_birth("121256-123S")
 #' @export
 #' @importFrom stringi stri_sub
-#' @importFrom lubridate dmy
+#' @importFrom dplyr case_when
 
 calc_birth <- function(x) {
-  output <- ifelse(is.na(x), NA,
-    ifelse(stri_sub(x, 7, 7) == "-",
-      paste0(stri_sub(x, 1, 4), "19", stri_sub(x, 5, 6)),
-      paste0(stri_sub(x, 1, 4), "20", stri_sub(x, 5, 6))))
 
-  output <- dmy(output)
+  # TODO: Check that it is correct format
 
-  return(output)
+  # Substract elements
+  separator <- stri_sub(x, 7, 7)
+  dm <- stri_sub(x, 1, 4)
+  y <- stri_sub(x, 5, 6)
+
+  # Add a century or two based on conditions
+  Y <- case_when(
+    separator == "-" ~paste0("19", y),
+    separator == "A" ~paste0("20", y),
+    separator == "+" ~paste0("18", y),
+    TRUE ~ NA_character_)
+
+  # Parse to date
+  as.Date(paste0(dm, Y), format = "%d%m%Y")
 }
