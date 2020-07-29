7b6acca8a5b8de1da7b389e52ec94a268ff71544
Jouni Vatanen <jounipvatanen@gmail.com>
2020-07-29 14:52:19 +0300

    send_error_sms function

--- R/send_error_mail.R
+++ R/send_error_mail.R
@@ -1,44 +1,3 @@
-#' send_mail
-#'
-#' Send email.
-#' @param to email or a vector of emails.
-#' @param subject of an email.
-#' @param body of en email.
-#' @param user user of sender's email account
-#' @param password password of sender's email account
-#' @param smtp smtp. If NULL, then gmail.
-#' @keywords email
-#' @examples
-#' \dontrun{
-#'to <- "address@email.com"
-#'subject <- "You have mail"
-#'body <- "Hi,
-#'you received a mail.."
-#'user <- "erroraddress@email.com"
-#'password <- "1234567"
-#'send_mail(to, subject, body, user, password)
-#'}
-#' @importFrom mailR send.mail
-#' @export
-
-# Send mail
-send_mail <- function(
-  to, subject = "", body = "", user, password, smtp = NULL) {
-
-  # Create gmail setting, if smtp is missing
-  if (is.null(smtp)) {
-    host.name <- "smtp.gmail.com"
-    port <- 465
-    ssl <- TRUE
-    }
-
-  send.mail(
-    from = user, to = to, subject = subject, body = body, smtp = list(
-      host.name = host.name, port = port, ssl = ssl, user.name = user,
-      passwd = password),
-    authenticate = TRUE, send = TRUE)
-}
-
 #' send_error_mail
 #'
 #' Send email on error.
@@ -63,13 +22,30 @@ send_mail <- function(
 #' @export
 
 # Send error mail
-send_error_mail <- function(to, subject, body, user, password, smtp = NULL) {
+send_error_mail <- function(to, user, password, smtp = NULL) {
+
+  # Send email on error
   options(error = function() {
-    send_mail(
-      to = to, subject = "Alert!",
-      body = paste(
-        "R command failed in path:", getwd(),
-        "and with error message:", geterrmessage()),
-      user = user, password = password, smtp = smtp)
+
+    # Create gmail setting, if smtp is missing
+    if (is.null(smtp)) {
+      host.name <- "smtp.gmail.com"
+      port <- 465
+      ssl <- TRUE
+    }
+
+    subject = "Alert!"
+
+    body <- paste(
+      "R command failed in path:", getwd(),
+      "and with error message:", geterrmessage())
+
+    send.mail(
+      from = user, to = to, subject = subject,
+      body = body,
+      smtp = list(
+        host.name = host.name, port = port, ssl = ssl, user.name = user,
+        passwd = password),
+      authenticate = TRUE, send = TRUE)
     })
 }
